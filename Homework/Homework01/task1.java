35e6db0239854243072a5ec20d310fc16f1a6609
Borodin <Fallenlord48@gmail.com>
2023-03-02 19:41:00 +0300

    Создание папки с домашними заданиями

---
+++ Homework/Homework01/task1.java
@@ -0,0 +1,25 @@
+package Homework.Homework01;
+/**
+ Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+  n! (произведение чисел от 1 до n)
+ */
+import java.util.*;
+public class task1 {
+    public static void main(String[] args) {
+        System.out.print("Введите любое целое положительное число : ");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+        int snum = 1;
+        int factor = 1;
+        for (int i = 2; i <= num; i++){
+            snum = snum + i;
+            factor = factor*i;  
+            }    
+         
+        System.out.println ("Введенное число " + num);
+        System.out.println ("n-ое треугольного число = " + snum);
+        System.out.println ("Факториал = " + factor);
+    }
+    
+}
