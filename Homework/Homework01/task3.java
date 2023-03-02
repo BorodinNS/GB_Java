d65871fa404c60e516b9b9ebc6b342cfbc6a305b
Borodin <Fallenlord48@gmail.com>
2023-03-02 19:46:22 +0300

    Выполнение первого ДЗ

---
+++ Homework/Homework01/task3.java
@@ -0,0 +1,38 @@
+package Homework.Homework01;
+
+/**
+
+ * Реализовать простой калькулятор
+ */
+
+import java.util.*;
+public class task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите первое число: ");		
+		Double num1 = scan.nextDouble ();
+        System.out.print("Введите второе число: ");		
+		Double num2 = scan.nextDouble ();
+        
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Введите операцию: ");
+		String operation = scan1.nextLine();        
+       
+        switch (operation){
+            case "+":
+                System.out.println(num1 + " + " +num2 + " = " + (num1+num2));
+                break;
+            case "-":
+                System.out.println(num1 + " - " +num2 + " = " + (num1-num2));
+                break;
+            case "*":
+                System.out.println(num1 + " * " +num2 + " = " + (num1*num2));
+                break;
+            case "/":
+                System.out.println(num1 + " / " +num2 + " = " + (num1/num2));
+                break;
+            default:
+                System.out.println("нет такой операции :(");
+        }
+   }    
+}
