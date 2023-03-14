990dd5eefab0f79be361a1f8705f4c7fda538cbb
Borodin <Fallenlord48@gmail.com>
2023-03-14 14:22:34 +0300

    ДЗ 6. Итоговая работа. Реализация каталога ноутбуков с помощью Java. Сортировка по нескольким параметрам.

---
+++ Homework/Homework06/Catalog.java
@@ -0,0 +1,9 @@
+package Homework.Homework06;
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
